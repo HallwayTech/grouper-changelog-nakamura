dc74527f332cf4bcbd229145ec5cfd82135d2212
Erik Froese <erik.froese@gmail.com>
2011-06-08 13:59:51 -0400

    dd a test for the group structure.

--- src/test/java/org/sakaiproject/nakamura/grouper/changelog/TemplateGroupIdAdapterTestCase.java
+++ src/test/java/org/sakaiproject/nakamura/grouper/changelog/TemplateGroupIdAdapterTestCase.java
@@ -10,12 +10,24 @@ public class TemplateGroupIdAdapterTestCase extends TestCase {
 	
 	@Override
 	public void setUp(){
-		groupIdAdapter = new TemplateGroupIdAdapter("edu:apps:sakaioae:([^:]+):([^:]+):([^:]+)$", 
-													"'newgroup_' + g[0] + '_' + g[1] + '_' + g[2]");
+		
 	}
 
 	public void testGetNakamuraName(){
+		String pattern = "edu:apps:sakaioae:([^:]+):([^:]+):([^:]+)$";
+		String template = "'newgroup_' + g[0] + '_' + g[1] + '_' + g[2]";
+		groupIdAdapter = new TemplateGroupIdAdapter(pattern, template);
 		assertEquals("newgroup_some_thing_else", 
 				groupIdAdapter.getNakamuraGroupId("edu:apps:sakaioae:some:thing:else"));
+		
+	}
+	
+	public void testGetNakamuraNameCourseStructure(){
+		String pattern = "edu:apps:sakaioae:([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^:]+)$";
+		String template = "g[0] + '_' + g[1] + '_' + g[2] + '_' + g[3] + '_' + g[4] + '_' + g[5]";
+		groupIdAdapter = new TemplateGroupIdAdapter(pattern, template); 
+													
+		assertEquals("acad_term_subject_catalog_session_section", 
+				groupIdAdapter.getNakamuraGroupId("edu:apps:sakaioae:acad:term:subject:catalog:session:section"));
 	}
 }
