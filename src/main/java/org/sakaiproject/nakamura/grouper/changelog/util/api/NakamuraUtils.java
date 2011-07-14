8330ec3685f60ccca6f9ea8da4fb76384f913665
Erik Froese <erik.froese@gmail.com>
2011-07-14 09:27:14 -0400

    Add a method to get the psuedo group suffixes from the config file.

--- src/main/java/org/sakaiproject/nakamura/grouper/changelog/util/api/NakamuraUtils.java
+++ src/main/java/org/sakaiproject/nakamura/grouper/changelog/util/api/NakamuraUtils.java
@@ -1,5 +1,28 @@
 package org.sakaiproject.nakamura.grouper.changelog.util.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+
+import edu.internet2.middleware.grouper.app.loader.GrouperLoaderConfig;
+
 public class NakamuraUtils {
-	public static String PROPERTY_KEY_PREFIX = "nakamura";
+
+	public static final String PROPERTY_KEY_PREFIX = "nakamura";
+
+	private static Set<String> psuedoGroupSuffixes = new HashSet<String>();
+
+	/**
+	 * @return the pseudo group suffixes from sakai
+	 * TODO: Find a better way to get these values. It'd be nice if sakai
+	 * stored them somewhere available via HTTP.
+	 */
+	public static Set<String> getPsuedoGroupSuffixes(){
+		String str = GrouperLoaderConfig.getPropertyString(PROPERTY_KEY_PREFIX + ".psuedoGroup.suffixes");
+		for(String suffix: StringUtils.split(str, ",")){
+			psuedoGroupSuffixes.add(suffix.trim());
+		}
+		return psuedoGroupSuffixes;
+	}
 }
