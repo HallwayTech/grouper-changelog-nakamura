cac35eb3646a72fdbb6bd45e80e68b961497d66d
Erik Froese <erik.froese@gmail.com>
2011-07-15 10:59:14 -0400

    Mornign code review. Extract constants and check for json requests by looking for the .json.

--- src/main/java/org/sakaiproject/nakamura/grouper/changelog/HttpCourseAdapter.java
+++ src/main/java/org/sakaiproject/nakamura/grouper/changelog/HttpCourseAdapter.java
@@ -28,18 +28,23 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 	private Log log = LogFactory.getLog(HttpCourseAdapter.class);
 
-	protected final String BATCH_URI         = "/system/batch";
-	protected final String CREATE_FILE_URI   = "/system/pool/createfile";
-	
-	private static final String DEFAULT_TEMPLATE = "mathcourse";
+	private static final String HTTP_REFERER = "/system/console/grouper";
+	private static final String HTTP_USER_AGENT = "Nakamura Grouper Sync";
+
+	protected static final String BATCH_URI         = "/system/batch";
+	protected static final String CREATE_FILE_URI   = "/system/pool/createfile";
+
+	private static final String DEFAULT_COURSE_TEMPLATE = "mathcourse";
+
+	private static final String BATCH_REQUESTS_PARAM = "requests";
 
 	/**
 	 * Create the full set of objects that are necessary to have a working
-	 * course in Sakai OAE. 
-	 * 
+	 * course in Sakai OAE.
+	 *
 	 * Written on the back of the amazing John King who pulled out all of this
 	 * for nakamura/testscripts/SlingRuby/dataload/full_group_creator.rb
-	 * @throws GroupModificationException 
+	 * @throws GroupModificationException
 	 */
 	@Override
 	public void createGroup(Group group) throws GroupModificationException {
@@ -78,11 +83,11 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 		method = new PostMethod(url + GROUP_CREATE_URI);
 		method.setParameter("_charset_", "utf-8");
 		method.setParameter(":name", parentGroupId);
-		method.setParameter("sakai:group-title", group.getDescription());
-		method.setParameter("sakai:group-description", group.getDescription());
+		method.setParameter("sakai:group-title", parentGroupId);
+		method.setParameter("sakai:group-description", parentGroupId);
 		method.setParameter("sakai:group-id", parentGroupId);
 		method.setParameter("sakai:category", "courses");
-		method.setParameter("sakai:templateid", DEFAULT_TEMPLATE);
+		method.setParameter("sakai:templateid", DEFAULT_COURSE_TEMPLATE);
 		method.setParameter("sakai:joinRole", "student");
 		method.setParameter("sakai:roles", "[{\"id\":\"student\",\"roleTitle\":\"Students\",\"title\":\"Student\",\"allowManage\":false},{\"id\":\"ta\",\"roleTitle\":\"Teaching Assistants\",\"title\":\"Teaching Assistant\",\"allowManage\":true},{\"id\":\"lecturer\",\"roleTitle\":\"Lecturers\",\"title\":\"Lecturer\",\"allowManage\":true}]");
 		try {
@@ -96,14 +101,14 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 		// --------------------------------------------------------------------
 		// POST 5 - updating the group managers
 		JSONArray batchPosts = new JSONArray();
-		
+
 		for (String managerGroupId: new String[] { taGroupId, lecturerGroupId }){
 			JSONObject request;
 			JSONObject params;
 			for (String roleGroupId: new String[] { studentGroupId, taGroupId, lecturerGroupId }){
 				request = new JSONObject();
 				params = new JSONObject();
-				// courseId-managerRoleName will be a manager of roleName 
+				// courseId-managerRoleName will be a manager of roleName
 				params.put(":manager", managerGroupId);
 				params.put("_charset_", "utf-8");
 				request.put("url", GROUP_PATH_PREFIX + "/" + roleGroupId + ".update.json");
@@ -127,14 +132,14 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 		method = new PostMethod(url + BATCH_URI);
 		JSONArray json = JSONArray.fromObject(batchPosts);
-		method.setParameter("requests", json.toString());
+		method.setParameter(BATCH_REQUESTS_PARAM, json.toString());
 		post(client, method);
 		log.debug("Updated the group managers.");
 
 		// --------------------------------------------------------------------
 		// POST 6 - updating the group members
 	    batchPosts.clear();
-	    
+
 	    JSONObject req1 = new JSONObject();
 		JSONObject p1 = new JSONObject();
 		p1.put(":member", creator);
@@ -172,7 +177,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 	    method = new PostMethod(url + BATCH_URI);
 	    json = JSONArray.fromObject(batchPosts);
-		method.setParameter("requests", json.toString());
+		method.setParameter(BATCH_REQUESTS_PARAM, json.toString());
 		post(client, method);
 		log.debug("Updated the group members.");
 
@@ -198,14 +203,14 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    syllabusP.put("structure0", "{\"week1\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"Week 1\",\"main\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"Week 1\"}},\"week2\":{\"_ref\":\"id569856425\",\"_title\":\"Week 2\",\"_order\":1,\"main\":{\"_ref\":\"id569856425\",\"_order\":0,\"_title\":\"Week 2\"}},\"week3\":{\"_ref\":\"id647321988\",\"_title\":\"Week 3\",\"_order\":2,\"main\":{\"_ref\":\"id647321988\",\"_order\":0,\"_title\":\"Week 3\"}}}");
 	    syllabusP.put("sakai:custom-mimetype", "x-sakai/document");
 	    syllabusP.put("_charset_", "utf-8");
-	    
+
 	    req1 = new JSONObject();
 	    req1.put("url", CREATE_FILE_URI);
 	    req1.put("method", "POST");
 	    req1.put("parameters", syllabusP);
 	    req1.put("_charset_", "utf-8");
 	    batchPosts.add(req1);
-	    
+
 	    JSONObject contactP = new JSONObject();
 	    contactP.put("sakai:pooled-content-file-name", "Contact us");
 	    contactP.put("sakai:description", "");
@@ -214,14 +219,14 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    contactP.put("structure0", "{\"about\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"About\",\"main\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"About\"}},\"prospective\":{\"_ref\":\"id373710599\",\"_title\":\"Prospective Students\",\"_order\":1,\"main\":{\"_ref\":\"id373710599\",\"_order\":0,\"_title\":\"Prospective Students\"}}}");
 	    contactP.put("sakai:custom-mimetype", "x-sakai/document");
 	    contactP.put("_charset_", "utf-8");
-	    
+
 	    JSONObject req2 = new JSONObject();
 	    req2.put("url", CREATE_FILE_URI);
 	    req2.put("method", "POST");
 	    req2.put("parameters", contactP);
 	    req2.put("_charset_", "utf-8");
 	    batchPosts.add(req2);
-	    
+
 	    JSONObject orgNotesP = new JSONObject();
 	    orgNotesP.put("sakai:pooled-content-file-name", "Organization Notes");
 	    orgNotesP.put("sakai:description", "");
@@ -230,14 +235,14 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    orgNotesP.put("structure0", "{\"organizationnotes\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"Organization Notes\",\"main\":{\"_ref\":\"id6573920372\",\"_order\":0,\"_title\":\"Organization Notes\"}}}");
 	    orgNotesP.put("sakai:custom-mimetype", "x-sakai/document");
 	    orgNotesP.put("_charset_", "utf-8");
-	    
+
 	    JSONObject req3 = new JSONObject();
 	    req3.put("url", CREATE_FILE_URI);
 	    req3.put("method", "POST");
 	    req3.put("parameters", orgNotesP);
 	    req3.put("_charset_", "utf-8");
 	    batchPosts.add(req3);
-	    
+
 	    JSONObject wikiP = new JSONObject();
 	    wikiP.put("sakai:pooled-content-file-name", "Student Wiki");
 	    wikiP.put("sakai:description", "");
@@ -246,7 +251,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    wikiP.put("structure0", "{\"studentwiki\":{\"_ref\":\"id849031890418\",\"_order\":0,\"_title\":\"Student Wiki\",\"main\":{\"_ref\":\"id849031890418\",\"_order\":0,\"_title\":\"Organization Notes\"}}}");
 	    wikiP.put("sakai:custom-mimetype", "x-sakai/document");
 	    wikiP.put("_charset_", "utf-8");
-	    
+
 	    JSONObject req4 = new JSONObject();
 	    req4.put("url", CREATE_FILE_URI);
 	    req4.put("method", "POST");
@@ -256,7 +261,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 	    method = new PostMethod(url + BATCH_URI);
 	    json = JSONArray.fromObject(batchPosts);
-	    method.setParameter("requests", json.toString());
+	    method.setParameter(BATCH_REQUESTS_PARAM, json.toString());
 	    JSONObject post8Response = post(client, method);
 
 	    // Go through the response and get the document UUID for the library and participants items.
@@ -264,7 +269,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    String contactUsDocHash = null;
 	    String orgNotesDocHash = null;
 	    String studentWikiDocHash = null;
-	    
+
 	    if (post8Response != null){
 	    	try {
 	    		JSONArray docResults = post8Response.getJSONArray("results");
@@ -297,9 +302,9 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 	    // --------------------------------------------------------------------
 	    // POST 9 - importing sakai docs content
-	    
+
 	    batchPosts.clear();
-	    
+
 	    JSONObject syllabusParams = new JSONObject();
 	    syllabusParams.put(":operation", "import");
 	    syllabusParams.put(":contentType", "json");
@@ -311,13 +316,13 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    syllabusContent.put("id569856425", JSONSerializer.toJSON(ImmutableMap.of("page", "<p>Week 2</p>")));
 	    syllabusContent.put("id647321988", JSONSerializer.toJSON(ImmutableMap.of("page", "<p>Week 3</p>")));
 	    syllabusParams.put(":content", syllabusContent);
-	    
+
 	    JSONObject syllabusRequest = new JSONObject();
 	    syllabusRequest.put("url", "/p/" + syllabusDocHash + ".resource");
 	    syllabusRequest.put("method", "POST");
 	    syllabusRequest.put("parameters", syllabusParams);
 	    syllabusRequest.put("_charset", "utf-8");
-	    
+
 	    JSONObject contactParams = new JSONObject();
 	    contactParams.put(":operation", "import");
 	    contactParams.put(":contentType", "json");
@@ -327,13 +332,13 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    JSONObject contactContent = new JSONObject();
 	    contactContent.put("id6573920372", JSONSerializer.toJSON(ImmutableMap.of("page", "<p><strong>Contact Us</strong></p><p>16 Mill Lane<br>1st Floor<br>CB2 1SB Cambridge</p><p><img id='widget_googlemaps_id439704665' class='widget_inline' style='display: block; padding: 10px; margin: 4px;' src='/devwidgets/googlemaps/images/googlemaps.png' data-mce-src='/devwidgets/googlemaps/images/googlemaps.png' data-mce-style='display: block; padding: 10px; margin: 4px;' border='1'><br></p>")));
 	    contactParams.put(":content", contactContent);
-	    
+
 	    JSONObject contactRequest = new JSONObject();
 	    contactRequest.put("url", "/p/" + contactUsDocHash + ".resource");
 	    contactRequest.put("method", "POST");
 	    contactRequest.put("parameters", contactParams);
 	    contactRequest.put("_charset", "utf-8");
-	    
+
 	    JSONObject orgNotesParams = new JSONObject();
 	    orgNotesParams.put(":operation", "import");
 	    orgNotesParams.put(":contentType", "json");
@@ -344,13 +349,13 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    orgNotesContent.put("id6573920372", JSONSerializer.toJSON(ImmutableMap.of("page", "<p>This is some information about the course<br></p>")));
 	    orgNotesContent.put("id373710599", JSONSerializer.toJSON(ImmutableMap.of("page", "<p>This is some information for prospective students<br>")));
 	    orgNotesParams.put(":content", orgNotesContent);
-	    
+
 	    JSONObject orgNotesRequest = new JSONObject();
 	    orgNotesRequest.put("url", "/p/" + orgNotesDocHash + ".resource");
 	    orgNotesRequest.put("method", "POST");
 	    orgNotesRequest.put("_charset", "utf-8");
 	    orgNotesRequest.put("parameters", orgNotesParams);
-	    
+
 	    JSONObject wikiParams = new JSONObject();
 	    wikiParams.put(":operation", "import");
 	    wikiParams.put(":contentType", "json");
@@ -360,25 +365,25 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    JSONObject wikiContent = new JSONObject();
 	    wikiContent.put("id849031890418", JSONSerializer.toJSON(ImmutableMap.of("page", "<p>Student wiki editable by all members of this course<br></p>")));
 	    wikiParams.put(":content", wikiContent);
-	    
+
 	    JSONObject wikiRequest = new JSONObject();
 	    wikiRequest.put("url", "/p/" + studentWikiDocHash + ".resource");
 	    wikiRequest.put("method", "POST");
 	    wikiRequest.put("_charset", "utf-8");
 	    wikiRequest.put("parameters", wikiParams);
-	    
+
 	    batchPosts.add(syllabusRequest);
 	    batchPosts.add(contactContent);
 	    batchPosts.add(orgNotesRequest);
 	    batchPosts.add(wikiRequest);
-	    
+
 	    method = new PostMethod(url + BATCH_URI);
 	    json = JSONArray.fromObject(batchPosts);
-	    method.setParameter("requests", json.toString());
+	    method.setParameter(BATCH_REQUESTS_PARAM, json.toString());
 	    post(client, method);
-	    
+
 	    log.debug("Added initial content into the sakai documents.");
-	    
+
 	    // ----------------------------------------------------------------
 	    batchPosts.clear();
 	    JSONObject request1 = new JSONObject();
@@ -407,7 +412,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request3.put("method", "POST");
 	    request3.put("parameters", params3);
 	    batchPosts.add(request3);
-	    
+
 	    JSONObject request4 = new JSONObject();
 	    JSONObject params4 = new JSONObject();
 	    params4.put(":viewer@Delete", new String[] {"everyone","anonymous"});
@@ -416,7 +421,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request4.put("method", "POST");
 	    request4.put("parameters", params4);
 	    batchPosts.add(request4);
-	    
+
 	    JSONObject request5 = new JSONObject();
 	    JSONObject params5 = new JSONObject();
 	    params5.put("principalId", new String[] {"everyone","anonymous"});
@@ -425,7 +430,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request5.put("method", "POST");
 	    request5.put("parameters", params5);
 	    batchPosts.add(request5);
-	    
+
 	    JSONObject request6 = new JSONObject();
 	    JSONObject params6 = new JSONObject();
 	    params6.put(":viewer", new String[] {"everyone","anonymous"});
@@ -433,7 +438,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request6.put("method", "POST");
 	    request6.put("parameters", params6);
 	    batchPosts.add(request6);
-	    
+
 	    JSONObject request7 = new JSONObject();
 	    JSONObject params7 = new JSONObject();
 	    params7.put("principalId", new String[] {"everyone","anonymous"});
@@ -441,7 +446,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request7.put("method", "POST");
 	    request7.put("parameters", params7);
 	    batchPosts.add(request7);
-	    
+
 	    JSONObject request8 = new JSONObject();
 	    JSONObject params8 = new JSONObject();
 	    params8.put(":viewer@Delete", new String[] {"everyone","anonymous"});
@@ -449,7 +454,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request8.put("method", "POST");
 	    request8.put("parameters", params8);
 	    batchPosts.add(request8);
-	    
+
 	    JSONObject request9 = new JSONObject();
 	    JSONObject params9 = new JSONObject();
 	    params9.put("principalId", new String[] {"everyone","anonymous"});
@@ -458,12 +463,12 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 	    request9.put("method", "POST");
 	    request9.put("parameters", params9);
 	    batchPosts.add(request9);
-	    
+
 	    method = new PostMethod(url + BATCH_URI);
 	    json = JSONArray.fromObject(batchPosts);
-	    method.setParameter("requests", json.toString());
+	    method.setParameter(BATCH_REQUESTS_PARAM, json.toString());
 	    post(client, method);
-	    
+
 	    log.debug("Set ACLs on sakai documents.");
 	}
 
@@ -481,7 +486,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 		method.addParameter("grouper:name", group.getParentStemName() + ":" + nakamuraGroupId.substring(nakamuraGroupId.lastIndexOf("-") + 1));
 		post(client, method);
 	}
-	
+
 	/**
 	 * Return the authorizableId of the parent group for this group.
 	 * @param nakamuraGroupId
@@ -499,8 +504,8 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 
 	public JSONObject post(HttpClient client, PostMethod method) throws GroupModificationException {
 
-		method.setRequestHeader(HttpHeaders.USER_AGENT, "Nakamura Grouper Sync");
-		method.setRequestHeader(HttpHeaders.REFERER, "/system/console/grouper");
+		method.setRequestHeader(HttpHeaders.USER_AGENT, HTTP_USER_AGENT);
+		method.setRequestHeader(HttpHeaders.REFERER, HTTP_REFERER);
 
 		String errorMessage = null;
 		String responseString = null;
@@ -509,39 +514,49 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 		try{
 			int returnCode = client.executeMethod(method);
 			responseString = IOUtils.toString(method.getResponseBodyAsStream());
-			responseJSON = JSONObject.fromObject(responseString);
+
+			boolean isJSONRequest = method.getURI().toString().endsWith(".json");
+			if (isJSONRequest){
+				responseJSON = JSONObject.fromObject(responseString);
+			}
 
 			switch (returnCode){
-			
+
 			case HttpStatus.SC_OK: // 200
 				break;
 			case HttpStatus.SC_BAD_REQUEST: // 400
+			case HttpStatus.SC_UNAUTHORIZED: // 401
 			case HttpStatus.SC_FORBIDDEN: // 404
 			case HttpStatus.SC_INTERNAL_SERVER_ERROR: // 500
-				errorMessage = responseJSON.getString("status.message");
+				if (isJSONRequest){
+					errorMessage = responseJSON.getString("status.message");
+				}
+				else {
+					errorMessage = responseString;
+				}
 				break;
 			default:
-				errorMessage = "Unknown HTTP response " + returnCode; 
+				errorMessage = "Unknown HTTP response " + returnCode;
 				break;
 			}
 		}
 		catch (Exception e) {
-			errorMessage = "An exception occurred posting to Sakai OAE. " + e.toString();
-		} 
+			errorMessage = "An exception occurred communicatingSakai OAE. " + e.toString();
+		}
 		finally {
 			method.releaseConnection();
 		}
 
 		if (errorMessage != null){
-			if (log.isErrorEnabled()){ 
+			if (log.isErrorEnabled()){
 				log.error(errorMessage);
 			}
 			errorToException(responseJSON);
 		}
 		return responseJSON;
 	}
-	
-	private void errorToException(JSONObject response) throws GroupModificationException, GroupAlreadyExistsException {		
+
+	private void errorToException(JSONObject response) throws GroupModificationException, GroupAlreadyExistsException {
 		String message = response.getString("status.message");
 		if (message == null){
 			return;
@@ -550,7 +565,7 @@ public class HttpCourseAdapter extends HttpSimpleGroupAdapter {
 		if (message.startsWith("A principal already exists with the requested name")){
 			throw new GroupAlreadyExistsException(message);
 		}
-		
+
 		throw new GroupModificationException(message);
 	}
 }
