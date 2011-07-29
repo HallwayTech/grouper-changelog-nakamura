4ae8de9e6cd96bb28036b712523fdfdd9d54487b
Erik Froese <erik@nyu.edu>
2011-07-29 09:40:06 -0400

    Support addIncludeExclude component groups.

--- src/main/java/org/sakaiproject/nakamura/grouper/changelog/SimpleGroupIdAdapter.java
+++ src/main/java/org/sakaiproject/nakamura/grouper/changelog/SimpleGroupIdAdapter.java
@@ -1,42 +1,52 @@
 package org.sakaiproject.nakamura.grouper.changelog;
 
+import java.util.Set;
+
 import org.sakaiproject.nakamura.grouper.changelog.esb.SimpleGroupEsbConsumer;
 import org.sakaiproject.nakamura.grouper.changelog.util.api.GroupIdAdapter;
 
 public class SimpleGroupIdAdapter implements GroupIdAdapter {
 
 	private String provisionedSimpleGroupsStem;
+	private String adhocSimpleGroupsStem;
+
+	private Set<String> pseudoGroupSuffixes;
+	private Set<String> includeExcludeSuffixes;
 
 	@Override
 	public String getNakamuraGroupId(String grouperName) {
 		String nakamuraGroupId = null;
-		int lastColon = grouperName.lastIndexOf(':');
-		String stem = grouperName.substring(0, lastColon);
-		String extension = grouperName.substring(lastColon + 1);
-
-		if (extension.equals(SimpleGroupEsbConsumer.MANAGER_SUFFIX) || 
-				extension.equals(SimpleGroupEsbConsumer.MEMBER_SUFFIX)){
-			grouperName = stem;
-		}
-		else {
-			// OAE must have updated the list of role groups. ugh
-			return null;
-		}
 
 		if (grouperName.startsWith(provisionedSimpleGroupsStem)){
 			nakamuraGroupId = grouperName.substring(provisionedSimpleGroupsStem.length()).replaceAll(":", "_");
-			if (nakamuraGroupId.startsWith("_")){
-				nakamuraGroupId = nakamuraGroupId.substring(1);
+		} else if (grouperName.startsWith(provisionedSimpleGroupsStem)){
+			nakamuraGroupId = grouperName.substring(adhocSimpleGroupsStem.length() + 6).replaceAll(":", "_");
+		}
+		if (nakamuraGroupId.startsWith("_")){
+			nakamuraGroupId = nakamuraGroupId.substring(1);
+		}
+
+		// If the groupername ends in _SUFFIX we change that to -SUFFIX
+		for (String suffix: pseudoGroupSuffixes){
+			if (nakamuraGroupId.endsWith("_" + suffix)){
+				nakamuraGroupId = nakamuraGroupId.substring(0, nakamuraGroupId.lastIndexOf("_")) + "-" + suffix;
+				break;
+			}
+			// If the groupername ends in _SUFFIX_systemOfRecord we change that to -SUFFIX
+			for (String ieSuffix: includeExcludeSuffixes) {
+				if (nakamuraGroupId.endsWith("_" + suffix + ieSuffix)){
+					nakamuraGroupId = nakamuraGroupId.substring(0, nakamuraGroupId.lastIndexOf("_"));
+					int newlast = nakamuraGroupId.lastIndexOf("_");
+					nakamuraGroupId = nakamuraGroupId.substring(0,newlast) + "-" + nakamuraGroupId.substring(newlast + 1);
+					break;
+				}
 			}
 		}
 
-		if (extension.equals(SimpleGroupEsbConsumer.MANAGER_SUFFIX)){
-			nakamuraGroupId += "-" + SimpleGroupEsbConsumer.MANAGER_SUFFIX;
+		if (!nakamuraGroupId.endsWith(SimpleGroupEsbConsumer.MANAGER_SUFFIX) &&
+			!nakamuraGroupId.endsWith(SimpleGroupEsbConsumer.MEMBER_SUFFIX)){
+			return null;
 		}
-		else {
-			nakamuraGroupId += "-" + SimpleGroupEsbConsumer.MEMBER_SUFFIX;
-		}
-
 		return nakamuraGroupId;
 	}
 
@@ -44,4 +54,15 @@ public class SimpleGroupIdAdapter implements GroupIdAdapter {
 		this.provisionedSimpleGroupsStem = simpleGroupsStem;
 	}
 
+	public void setAdhocSimpleGroupsStem(String adhocGroupsStem) {
+		this.adhocSimpleGroupsStem = adhocGroupsStem;
+	}
+
+	public void setPseudoGroupSuffixes(Set<String> pseudoGroupSuffixes) {
+		this.pseudoGroupSuffixes = pseudoGroupSuffixes;
+	}
+
+	public void setIncludeExcludeSuffixes(Set<String> includeExcludeSuffixes) {
+		this.includeExcludeSuffixes = includeExcludeSuffixes;
+	}
 }
--- src/test/java/org/sakaiproject/nakamura/grouper/changelog/SimpleGroupIdAdapterTestCase.java
+++ src/test/java/org/sakaiproject/nakamura/grouper/changelog/SimpleGroupIdAdapterTestCase.java
@@ -1,30 +1,43 @@
 package org.sakaiproject.nakamura.grouper.changelog;
 
-import org.sakaiproject.nakamura.grouper.changelog.esb.SimpleGroupEsbConsumer;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
-public class SimpleGroupIdAdapterTestCase extends TestCase {
+import org.sakaiproject.nakamura.grouper.changelog.esb.SimpleGroupEsbConsumer;
 
-	private String PROVISIONED_STEM = "edu:apps:sakaioae:adhoc:groups";
+import com.google.common.collect.ImmutableSet;
+
+public class SimpleGroupIdAdapterTestCase extends TestCase  {
+
+	private Set<String> pseudoGroupSuffixes = ImmutableSet.of("member", "manager");
+	private Set<String> includeExcludeSuffixes = ImmutableSet.of("_includes", "_excludes", "_systemOfRecord", "_systemOfRecordAndIncludes");
+	private String PROVISIONED_STEM = "edu:apps:sakaioae:provisioned:simplegroups";
+	private String ADHOC_STEM = "edu:apps:sakaioae:adhoc:simplegroups";
+
+	SimpleGroupIdAdapter adapter;
+
+	@Override
+	public void setUp(){
+		adapter = new SimpleGroupIdAdapter();
+		adapter.setPseudoGroupSuffixes(pseudoGroupSuffixes);
+		adapter.setIncludeExcludeSuffixes(includeExcludeSuffixes);
+		adapter.setProvisionedSimpleGroupsStem(PROVISIONED_STEM);
+		adapter.setAdhocSimpleGroupsStem(ADHOC_STEM);
+
+	}
 
 	public void testGetNakamuraIdMembers(){
-		SimpleGroupIdAdapter adaptor = new SimpleGroupIdAdapter();
-		adaptor.setProvisionedSimpleGroupsStem(PROVISIONED_STEM);
 		assertEquals("newgroup_some_thing_else-" + SimpleGroupEsbConsumer.MEMBER_SUFFIX,
-				adaptor.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:" + SimpleGroupEsbConsumer.MEMBER_SUFFIX));
+				adapter.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:" + SimpleGroupEsbConsumer.MEMBER_SUFFIX));
 	}
-	
+
 	public void testGetNakamuraIdManagers(){
-		SimpleGroupIdAdapter adaptor = new SimpleGroupIdAdapter();
-		adaptor.setProvisionedSimpleGroupsStem(PROVISIONED_STEM);
 		assertEquals("newgroup_some_thing_else-" + SimpleGroupEsbConsumer.MANAGER_SUFFIX,
-				adaptor.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:" + SimpleGroupEsbConsumer.MANAGER_SUFFIX));
+				adapter.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:" + SimpleGroupEsbConsumer.MANAGER_SUFFIX));
 	}
-	
+
 	public void testGetNakamuraIdInvalid(){
-		SimpleGroupIdAdapter adaptor = new SimpleGroupIdAdapter();
-		adaptor.setProvisionedSimpleGroupsStem(PROVISIONED_STEM);
-		assertNull(adaptor.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:sssss"));
+		assertNull(adapter.getNakamuraGroupId(PROVISIONED_STEM + ":newgroup:some:thing:else:sssss"));
 	}
 }
