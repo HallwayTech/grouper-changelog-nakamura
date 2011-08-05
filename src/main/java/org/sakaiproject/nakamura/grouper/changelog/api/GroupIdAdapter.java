d094a0cfec9ec153781253682aca10161c4bb0df
Erik Froese <erik.froese@gmail.com>
2011-08-05 12:34:49 -0400

    I must have messed this file up when I committed. Re-fix API

--- src/main/java/org/sakaiproject/nakamura/grouper/changelog/api/GroupIdAdapter.java
+++ src/main/java/org/sakaiproject/nakamura/grouper/changelog/api/GroupIdAdapter.java
@@ -1,4 +1,4 @@
-package org.sakaiproject.nakamura.grouper.changelog.util.api;
+package org.sakaiproject.nakamura.grouper.changelog.api;
 
 /**
  * A strategy to map names from grouper to Nakamura.
@@ -10,7 +10,7 @@ public interface GroupIdAdapter {
 	 * @param grouperName the full group id in grouper ex: stem1:stem2:groupName
 	 * @return the group ID in nakamura ex: stem1_stem2_groupName
 	 */
-	public String getNakamuraGroupId(String grouperName);
+	public String getGroupId(String grouperName);
 
 	/**
 	 * Given a nakamura group id, return the parent of this group.
