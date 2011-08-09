93bf2438d477ac3a4f1be0d3a73a73ff7c39193f
Erik Froese <erik@nyu.edu>
2011-08-09 10:11:34 -0400

    Add tests for the title consumer. Just tests ignoreChangelogEntry right now.

---
+++ src/test/java/org/sakaiproject/nakamura/grouper/changelog/esb/CourseTitleEsbConsumerTest.java
@@ -0,0 +1,80 @@
+package org.sakaiproject.nakamura.grouper.changelog.esb;
+
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
+import java.util.regex.Pattern;
+
+import junit.framework.TestCase;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.sakaiproject.nakamura.grouper.changelog.GroupIdAdapterImpl;
+import org.sakaiproject.nakamura.grouper.changelog.HttpCourseAdapter;
+
+import edu.internet2.middleware.grouper.GroupFinder;
+import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.changeLog.ChangeLogEntry;
+import edu.internet2.middleware.grouper.changeLog.ChangeLogLabels;
+import edu.internet2.middleware.grouper.changeLog.ChangeLogProcessorMetadata;
+import edu.internet2.middleware.grouper.changeLog.ChangeLogTypeBuiltin;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(value = {GrouperUtil.class, GroupFinder.class, GrouperSession.class })
+public class CourseTitleEsbConsumerTest extends TestCase {
+
+	private CourseTitleEsbConsumer consumer;
+	private HttpCourseAdapter groupAdapter;
+	private GroupIdAdapterImpl groupIdAdapter;
+	private ChangeLogProcessorMetadata metadata;
+	private ChangeLogEntry entry;
+
+	private static final String VALID_STEM = "edu:apps:sakaioae:courses:some:stem";
+	private static final String INVALID_STEM = "edu:apps:sakaioae:courses:some:stem:extra";
+
+	public void setUp(){
+		groupAdapter = mock(HttpCourseAdapter.class);
+		groupIdAdapter = mock(GroupIdAdapterImpl.class);
+		metadata = mock(ChangeLogProcessorMetadata.class);
+		when(metadata.getConsumerName()).thenReturn("UnitTestConsumer");
+
+		consumer = new CourseTitleEsbConsumer();
+		consumer.setGroupAdapter(groupAdapter);
+		consumer.setGroupIdAdapter(groupIdAdapter);
+		consumer.setConfigurationLoaded(true);
+		consumer.setSectionStemPattern(Pattern.compile("edu:apps:sakaioae:courses:([^:]+):([^:]+)"));
+		suppress(method(GrouperUtil.class, "getLog"));
+
+		entry = mock(ChangeLogEntry.class);
+	}
+
+	public void testIgnoreInvalidEntryType() throws Exception{
+		when(entry.equalsCategoryAndAction(ChangeLogTypeBuiltin.STEM_UPDATE)).thenReturn(false);
+		assertTrue(consumer.ignoreChangelogEntry(entry));
+	}
+
+	public void testIgnoreDoesntMatchStemPattern(){
+		when(entry.equalsCategoryAndAction(ChangeLogTypeBuiltin.STEM_UPDATE)).thenReturn(true);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.STEM_UPDATE.name)).thenReturn(INVALID_STEM);
+		assertTrue(consumer.ignoreChangelogEntry(entry));
+	}
+
+	public void testIgnoreNotADescriptionChange(){
+		when(entry.equalsCategoryAndAction(ChangeLogTypeBuiltin.STEM_UPDATE)).thenReturn(true);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.STEM_UPDATE.name)).thenReturn(VALID_STEM);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.STEM_UPDATE.propertyChanged)).thenReturn("notdescription");
+		assertTrue(consumer.ignoreChangelogEntry(entry));
+	}
+
+	public void testDontIgnore(){
+		when(entry.equalsCategoryAndAction(ChangeLogTypeBuiltin.STEM_UPDATE)).thenReturn(true);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.STEM_UPDATE.name)).thenReturn(VALID_STEM);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.STEM_UPDATE.propertyChanged)).thenReturn("description");
+		assertFalse(consumer.ignoreChangelogEntry(entry));
+	}
+}
