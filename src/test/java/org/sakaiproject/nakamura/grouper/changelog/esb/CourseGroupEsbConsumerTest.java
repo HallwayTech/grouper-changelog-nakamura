45bd3206b749bea167cf147ab3a082a40bf93c3c
Erik Froese <erik.froese@gmail.com>
2011-08-09 00:04:26 -0400

    Test adding a group!

--- src/test/java/org/sakaiproject/nakamura/grouper/changelog/esb/CourseGroupEsbConsumerTest.java
+++ src/test/java/org/sakaiproject/nakamura/grouper/changelog/esb/CourseGroupEsbConsumerTest.java
@@ -1,9 +1,12 @@
 package org.sakaiproject.nakamura.grouper.changelog.esb;
 
 import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.support.membermodification.MemberMatcher.method;
 import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
 import junit.framework.TestCase;
 
 import org.junit.runner.RunWith;
@@ -11,7 +14,13 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.sakaiproject.nakamura.grouper.changelog.GroupIdAdapterImpl;
 import org.sakaiproject.nakamura.grouper.changelog.HttpCourseAdapter;
+import org.sakaiproject.nakamura.grouper.changelog.exceptions.GroupModificationException;
 
+import com.google.common.collect.ImmutableList;
+
+import edu.internet2.middleware.grouper.Group;
+import edu.internet2.middleware.grouper.GroupFinder;
+import edu.internet2.middleware.grouper.GrouperSession;
 import edu.internet2.middleware.grouper.changeLog.ChangeLogEntry;
 import edu.internet2.middleware.grouper.changeLog.ChangeLogLabels;
 import edu.internet2.middleware.grouper.changeLog.ChangeLogProcessorMetadata;
@@ -20,7 +29,7 @@ import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(value = {GrouperUtil.class})
+@PrepareForTest(value = {GrouperUtil.class, GroupFinder.class, GrouperSession.class })
 public class CourseGroupEsbConsumerTest extends TestCase {
 
 	private CourseGroupEsbConsumer consumer;
@@ -96,5 +105,34 @@ public class CourseGroupEsbConsumerTest extends TestCase {
 		when(groupIdAdapter.isCourseGroup(grouperName)).thenReturn(true);
 		when(groupIdAdapter.isInstitutional(grouperName)).thenReturn(true);
 		assertFalse(consumer.ignoreChangelogEntry(entry));
+	}
+
+	public void testAddGroup() throws GroupModificationException{
+		entry = mock(ChangeLogEntry.class);
+		String grouperName = "edu:apps:sakaiaoe:courses:some:course:students";
+		when(entry.equalsCategoryAndAction(ChangeLogTypeBuiltin.GROUP_ADD)).thenReturn(true);
+		when(entry.retrieveValueForLabel(ChangeLogLabels.GROUP_ADD.name)).thenReturn(grouperName);
+		when(groupIdAdapter.isCourseGroup(grouperName)).thenReturn(true);
+		when(groupIdAdapter.isInstitutional(grouperName)).thenReturn(false);
+		assertFalse(consumer.ignoreChangelogEntry(entry));
+
+		Group group = mock(Group.class);
+		when(group.getDescription()).thenReturn("description");
+		GrouperSession session = mock(GrouperSession.class);
+		mockStatic(GrouperSession.class);
+		mockStatic(GroupFinder.class);
+		when(GrouperSession.startRootSession()).thenReturn(session);
+		when(GroupFinder.findByName(session, grouperName, false)).thenReturn(group);
+
+		when(groupIdAdapter.getGroupId(grouperName)).thenReturn("some_course-student");
+		when(groupIdAdapter.getPseudoGroupParent("some_course-student")).thenReturn("some_course");
+		when(groupAdapter.groupExists("some_course")).thenReturn(false);
+		when(groupIdAdapter.getInstitutionalCourseGroupsStem()).thenReturn("no-match");
+
+		// Prevent GrouperLoaderConfig from staticing the test up
+		consumer.setConfigurationLoaded(true);
+		consumer.processChangeLogEntries(ImmutableList.of(entry), metadata);
+
+		verify(groupAdapter).createGroup(grouperName, "description");
 	}
 }
